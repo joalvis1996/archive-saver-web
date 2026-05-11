60e8ac43c3e0593864bac5ddee8d2b33d71c9bc9
alvisjo <lwalwakk1@naver.com>
2026-05-11 16:05:34 +0900

    Bump Android version to 0.3.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.archivesaver.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 3
+        versionName = "0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField(
